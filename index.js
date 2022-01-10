c94cb572d5dd8abe979a58f689929096ece1f23d
AM0726Github <Margaryanalik7@gmail.com>
2022-01-09 21:45:25 -0800

    finish questions part

--- index.js
+++ index.js
@@ -35,21 +35,79 @@ const promptUser = () => {
         }
       },
 
-      
+
       {
         type: 'input',
         name: 'Title',
-        message: "Enter project title : ",
+        message: 'Enter project title : ',
+        default: '',
+        validate: function (answer) {
+            if (!answer) {
+                return console.log("A valid enter is required");
+            }
+            return true;
+        }
       },
       {
         type: 'input',
-        name: "Description",
-        message: "Write a brief description of your project: "
+        name: 'Description',
+        message: 'Entree description to project : ',
+        default: '',
+        validate: function (answer) {
+            if (!answer) {
+                return console.log("A valid enter is required");
+            }
+            return true;
+        }
       },
       {
         type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:'
-      }
+        name: 'Installation',
+        message: 'Enter instalation process description : ',
+        default: '',
+        validate: function (answer) {
+            if (!answer) {
+                return console.log("A valid enter is required");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Enter project usage description : ',
+        default: '',
+        validate: function (answer) {
+            if (!answer) {
+                return console.log("A valid enter is required");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'Chose the license for project : ',
+        choices: [
+            'GNU AGPLv3',
+            'GNU GPLv3',
+            'GNU LGPLv3',
+            'Mozilla Public 2.0',
+            'Apache 2.0',
+            'MIT',
+            'Boost Software 1.0',
+            'The Unlicense'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'Enter contributing info : '
+    },
+    {
+        type: 'input',
+        name: 'Tests',
+        message: 'Enter test info : '
+    },
     ]);
   };
