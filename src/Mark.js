f6bb6b6fbed4c6f574d897c45f2b7150a3e08be9
AM0726Github <Margaryanalik7@gmail.com>
2022-01-10 00:02:28 -0800

    last apdate

--- src/Mark.js
+++ src/Mark.js
@@ -1,11 +1,11 @@
 // Generate the README.md
 function CreateReadme(answers) {
     return `
-<h1 align="center">${answers.Title} 👋</h1>
+<h1>${answers.Title}</h1>
   
-![badge](https://img.shields.io/badge/license-${answers.Description}-brightgreen)<br />
+![badge](https://img.shields.io/badge/license-${answers.Description}-green)<br />
 ## Description
-🔍 ${answers.Description}
+${answers.Description}
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
@@ -13,23 +13,22 @@ function CreateReadme(answers) {
 - [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [Questions](#questions)
 ## Installation
-💾 ${answers.Installation}
+ ${answers.Installation}
 ## Usage
-💻 ${answers.Usage}
+ ${answers.Usage}
 ## License
-![badge](https://img.shields.io/badge/license-${answers.License}-brightgreen)
+![badge](https://img.shields.io/badge/license-${answers.License}-green)
 <br />
 This application is covered by the ${answers.license} license. 
 ## Contributing
-👪 ${answers.Contributing}
+${answers.Contributing}
 ## Tests
-✏️ ${answers.Tests}
+${answers.Tests}
 
 :octocat: Find me on GitHub: [${answers.UserName}](https://github.com/${answers.UserName})<br />
 <br />
-_This README was generated with ❤️ by [README-generator](https://github.com/AM0726Github/NodeCPRG)`;
+_This README was generated with ❤️ by [Rgenerator](https://github.com/AM0726Github/NodeCPRG)`;
   };
   
   module.exports = CreateReadme;
